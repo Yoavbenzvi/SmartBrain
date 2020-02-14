83d5926fc895be8737249c2d085f71627e2a210e
YoavBZ <yoav.benzvi@gmail.com>
2020-02-14 18:06:51 +0100

    to add facebox

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+	apiKey: '2bcd9ce77860499e8ddcf71bd7d417a9'
+});
 
 const particlesOptions = {
 	particles: {
@@ -20,6 +26,45 @@ const particlesOptions = {
 }
 
 class App extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			input: '',
+			imageUrl: '',
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({
+			input: event.target.value
+		})
+	}
+
+	onButtonSubmit = () => {
+		this.setState({
+			imageUrl: this.state.input
+		})
+		app.models.predict(
+			Clarifai.FACE_DETECT_MODEL,
+			`${this.state.input}`
+		)
+			.then(function(response) {
+    			console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    		}, 
+    		function(err) {console.log('there was an error')}
+		);
+	}
+
+	//PREDICT new??
+	// app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+ 	//      .then(generalModel => {
+ 	//        return generalModel.predict("@@sampleTrain");
+ 	//      })
+ 	//      .then(response => {
+ 	//        var concepts = response['outputs'][0]['data']['concepts']
+ 	//      })
+
 	render() {
 		return(
 			<div className="tc">
@@ -27,7 +72,10 @@ class App extends React.Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm />
+				<ImageLinkForm 
+					onInputChange={this.onInputChange} 
+					onButtonSubmit={this.onButtonSubmit}/>
+				<FaceRecognition imageUrl={this.state.imageUrl}/>
 			</div>
 		)
 	}
