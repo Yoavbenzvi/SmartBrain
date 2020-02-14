4caabc19c325b5fb07644e8c6ba41b78a20c93ce
YoavBZ <yoav.benzvi@gmail.com>
2020-02-14 16:05:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+	particles: {
+		number: {
+			//amount of polygons
+			value: 100,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+    }
+}
+
+class App extends React.Component {
+	render() {
+		return(
+			<div className="tc">
+				<Particles params={particlesOptions} className='particles'/>
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm />
+			</div>
+		)
+	}
 }
 
 export default App;
