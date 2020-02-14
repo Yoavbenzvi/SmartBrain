87f7025460aee3af884b1aa1659e799c5bf7b59f
YoavBZ <yoav.benzvi@gmail.com>
2020-02-14 20:12:59 +0100

    next sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import SignIn from './components/SignIn/SignIn';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
@@ -32,7 +33,28 @@ class App extends React.Component {
 		this.state = {
 			input: '',
 			imageUrl: '',
+			box: {},
 		}
+	}
+
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputimage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - (clarifaiFace.right_col * width),
+			bottomRow: height - (clarifaiFace.bottom_row * height),
+		}
+	}
+
+	displayFaceBox = (box) => {
+		console.log(box);
+		this.setState({
+			box: box
+		})
 	}
 
 	onInputChange = (event) => {
@@ -45,37 +67,23 @@ class App extends React.Component {
 		this.setState({
 			imageUrl: this.state.input
 		})
-		app.models.predict(
-			Clarifai.FACE_DETECT_MODEL,
-			`${this.state.input}`
-		)
-			.then(function(response) {
-    			console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    		}, 
-    		function(err) {console.log('there was an error')}
-		);
+		app.models.predict(Clarifai.FACE_DETECT_MODEL,`${this.state.input}`)
+			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+			.catch(err => console.log(err));
 	}
-
-	//PREDICT new??
-	// app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
- 	//      .then(generalModel => {
- 	//        return generalModel.predict("@@sampleTrain");
- 	//      })
- 	//      .then(response => {
- 	//        var concepts = response['outputs'][0]['data']['concepts']
- 	//      })
 
 	render() {
 		return(
 			<div className="tc">
 				<Particles params={particlesOptions} className='particles'/>
 				<Navigation />
+				<SignIn />
 				<Logo />
 				<Rank />
 				<ImageLinkForm 
 					onInputChange={this.onInputChange} 
 					onButtonSubmit={this.onButtonSubmit}/>
-				<FaceRecognition imageUrl={this.state.imageUrl}/>
+				<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
 			</div>
 		)
 	}
