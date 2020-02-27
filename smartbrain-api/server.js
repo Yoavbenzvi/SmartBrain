2a1de6e9880c05d3aca02954693d20082b8b5e12
YoavBZ <yoav.benzvi@gmail.com>
2020-02-27 12:34:54 +0100

    added hash password using db (synchronus) in the register path. added a transaction query to register. table login now holds password hashes. changed the signin path in server to check hash against database. and return user if true.

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
+const app = express();
 
 const db = knex({
   client: 'pg',
@@ -13,56 +14,56 @@ const db = knex({
   }
 });
 
-const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'john',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'sally',
-			email: 'sally@gmail.com',
-			password: 'bannanas',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-}
 
 app.get('/', (req, res) => {
 	res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-	if(req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('error logging in');
-	}
+	db.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if(isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+			} else {
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users')
-		.returning('*')
-		.insert({
-			email: email,
-			name: name,
-			joined: new Date()
-		})
-		.then(user => {
-			res.json(user[0]);
+	const hash = bcrypt.hashSync(password);
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+				.returning('*')
+				.insert({
+					email: loginEmail[0],
+					name: name,
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0])
+				})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
 		})
 		.catch(err => res.status(400).json('unable to register'))
 })
@@ -90,18 +91,6 @@ app.put('/image', (req, res) => {
 	})
 	.catch(err => res.status(400).json('unable to get entries'))
 })
-
-
-// bcrypt.hash(password, null, null, function(err, hash) {
-//   		console.log(hash);
-// });
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
 
 app.listen(3001, () => {
 	console.log('app is  running on port 3000');
