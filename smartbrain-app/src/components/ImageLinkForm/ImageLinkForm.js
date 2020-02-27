2f1c1d2a8904fce8e4938ae577c3d519a4b12653
YoavBZ <yoav.benzvi@gmail.com>
2020-02-27 16:12:28 +0100

    created a controllers folder, moved corresponding functions to files and cleaned up server.js

--- smartbrain-app/src/components/ImageLinkForm/ImageLinkForm.js
+++ smartbrain-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return(
 		<div>
 			<p className="f3">
-				{'This Magic Brain will detect faces and your picturs'}
+				{'This Magic Brain will detect faces in your picturs'}
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
