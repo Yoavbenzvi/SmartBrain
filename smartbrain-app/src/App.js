7205836c89855dd916161abf5131780b6238d284
YoavBZ <yoav.benzvi@gmail.com>
2020-02-29 12:16:31 +0100

    fixed a bug where an empty registration form could be submitted. moved clarifai function and ey to server for security reasons and created a new imageurl path for it

--- smartbrain-app/src/App.js
+++ smartbrain-app/src/App.js
@@ -7,12 +7,7 @@ import SignIn from './components/SignIn/SignIn';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
-
-const app = new Clarifai.App({
-	apiKey: '2bcd9ce77860499e8ddcf71bd7d417a9'
-});
 
 const particlesOptions = {
 	particles: {
@@ -91,7 +86,14 @@ class App extends React.Component {
 		this.setState({
 			imageUrl: this.state.input
 		})
-		app.models.predict(Clarifai.FACE_DETECT_MODEL,`${this.state.input}`)
+		fetch('http://localhost:3001/imageurl', {
+			method: 'post',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				input: this.state.input
+			})
+		})
+			.then(response => response.json())
 			.then(response => {
 				if(response) {
 					fetch('http://localhost:3001/image', {
