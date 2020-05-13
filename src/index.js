55949029dd1fae88cf34fb0f8b8bb54e27f5a938
Yoav <yoav.benzvi@gmail.com>
2020-05-13 13:37:12 +0200

    PWA
    -added serviceworker
    -added favicon and png's for manifest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker'
 import './index.css';
 import App from './App';
 import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register()
